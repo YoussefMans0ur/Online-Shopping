cb49b6e1516c8071fbc63b188075946ee282e0ff
YoussefMans0ur <youssefmansourr8111@gmail.com>
2024-05-08 16:39:14 +0300

    fourth commit

--- js/main.js
+++ js/main.js
@@ -1,26 +1,34 @@
 $(document).ready( function() {
 
-    let cartCounter = 0;
-    let cartCounterElement = document.getElementById('cart-counter');
+    $(document).on('click', '.btn-add, .btn-minus', function() {
+
+      var counterElement = $(this).closest('.counter-plus-minus').find('#cart-counter');
+      var cartCounter = parseInt(counterElement.text());
+
+      if ($(this).hasClass('btn-add')) {
+          cartCounter++;
+      } else if ($(this).hasClass('btn-minus')) {
+          if ($(this).hasClass('cart_item'))
+          {
+            if (cartCounter === 1) {
+                $(this).closest('.counter-plus-minus').find('.btn-minus').css('cursor', 'not-allowed');
+              } else {
+                $(this).closest('.counter-plus-minus').find('.btn-minus').css('cursor', 'pointer');
+                cartCounter = Math.max(1, cartCounter - 1);
+              }
+          }
+          else {
+            if (cartCounter === 0) {
+              $(this).closest('.counter-plus-minus').find('.btn-minus').css('cursor', 'not-allowed');
+            } else {
+              $(this).closest('.counter-plus-minus').find('.btn-minus').css('cursor', 'pointer');
+              cartCounter = Math.max(0, cartCounter - 1);
+            }
+          }
+      }
+      counterElement.text(cartCounter);
+  });
   
-    let addToCartButtons = document.querySelectorAll('.btn-add');
-    addToCartButtons.forEach(function(button) {
-      button.addEventListener('click', function() {
-        cartCounter++;
-        cartCounterElement.textContent = cartCounter;
-      });
-    });
-  
-    let removeFromCartButtons = document.querySelectorAll('.btn-minus');
-    removeFromCartButtons.forEach(function(button) {
-      button.addEventListener('click', function() {
-        if (cartCounter > 0) {
-          cartCounter--;
-          cartCounterElement.textContent = cartCounter;
-        }
-      });
-    });
-    
     $(".heart").click(function() {
       $(this).toggleClass("heart heart-des");
     });
